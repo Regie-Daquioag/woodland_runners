96b000ccb7e1b46a3861d3971f5734224181eabc
Regie-Daquioag <rdaquiong@csumb.edu>
2018-09-13 10:40:58 -0700

    seperated the index,js into multiple js files

--- index.js
+++ index.js
@@ -47,121 +47,6 @@ function Drawable() {
 }
 
 /**
- * Creates the Background object which will become a child of
- * the Drawable object. The background is drawn on the "background"
- * canvas and creates the illusion of moving by panning the image.
- */
-function Background() {
-	this.speed = 1; // Redefine speed of the background for panning
-	// Implement abstract function
-	this.draw = function() {
-		///************************************************************************
-		// // Pan background
-		this.x += this.speed;
-		// this.context.drawImage(imageRepository.background, this.x, this.y);
-		// // Draw another image at the top edge of the first image
-		// this.context.drawImage(imageRepository.background, this.x-this.canvasWidth, this.y);
-		// // If the image scrolled off the screen, reset
-		// if (this.x >= this.canvasWidth)
-		// 	this.x = 0;
-		///************************************************************************
-
-		// this.context.clearRect(0,0,this.canvasWidth,this.canvasHeight);
-		// this.context.fillStyle = '#333';
-		// this.context.fillRect(0,0,this.canvasWidth,this.canvasHeight);
-		this.context.drawImage(imageRepository.background, this.x, this.y);
-		this.context.drawImage(imageRepository.background, this.canvasWidth-Math.abs(this.x), this.y);
-		if(Math.abs(this.x) > this.canvasWidth){this.x = 0;}
-		this.x -= 10;
-		///************************************************************************
-
-
-	};
-}
-// Set Background to inherit properties from Drawable
-Background.prototype = new Drawable();
-
-/**
- * Create the animal object that the player controls. The animal is
- * drawn on the "animal" canvas and uses dirty rectangles to move
- * around the screen.
- */
-function Animal() {
-	this.speed = 3;
-	var counter = 0;
-	this.draw = function() {
-		// is this the position of where the animal wil be
-		this.context.drawImage(imageRepository.animal, this.x, this.y,this.width,this.height);
-	};
-	this.move = function() {
-		counter++;
-		// Determine if the action is move action
-		if (KEY_STATUS.left || KEY_STATUS.right ||
-			KEY_STATUS.down || KEY_STATUS.up) {
-			// The animal moved, so erase it's current image so it can
-			// be redrawn in it's new location
-			this.context.clearRect(this.x, this.y, this.width, this.height);
-			if (KEY_STATUS.up) {
-				this.y -= this.speed
-				if (this.y <= this.canvasHeight/4*3)
-					this.y = this.canvasHeight/4*3;
-			} else if (KEY_STATUS.down) {
-				this.y += this.speed
-				if (this.y >= this.canvasHeight - this.height)
-					this.y = this.canvasHeight - this.height;
-			}
-			// Finish by redrawing the animal
-			this.draw();
-		}
-	};
-}
-Animal.prototype = new Drawable();
-
-
-// The keycodes that will be mapped when a user presses a button.
-// Original code by Doug McInnes
-KEY_CODES = {
-  38: 'up',
-  40: 'down',
-}
-// Creates the array to hold the KEY_CODES and sets all their values
-// to false. Checking true/flase is the quickest way to check status
-// of a key press and which one was pressed when determining
-// when to move and which direction.
-KEY_STATUS = {};
-for (code in KEY_CODES) {
-  KEY_STATUS[ KEY_CODES[ code ]] = false;
-}
-/**
- * Sets up the document to listen to onkeydown events (fired when
- * any key on the keyboard is pressed down). When a key is pressed,
- * it sets the appropriate direction to true to let us know which
- * key it was.
- */
-document.onkeydown = function(e) {
-  // Firefox and opera use charCode instead of keyCode to
-  // return which key was pressed.
-  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-  if (KEY_CODES[keyCode]) {
-    e.preventDefault();
-    KEY_STATUS[KEY_CODES[keyCode]] = true;
-  }
-}
-/**
- * Sets up the document to listen to ownkeyup events (fired when
- * any key on the keyboard is released). When a key is released,
- * it sets teh appropriate direction to false to let us know which
- * key it was.
- */
-document.onkeyup = function(e) {
-  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-  if (KEY_CODES[keyCode]) {
-    e.preventDefault();
-    KEY_STATUS[KEY_CODES[keyCode]] = false;
-  }
-}
-
-/**
 * Creates the Game object which will hold all objects and data for
 * the game.
 */
@@ -211,6 +96,7 @@ function Game() {
 	 animate();
  };
 }
+
 /**
 * The animation loop. Calls the requestAnimationFrame shim to
 * optimize the game loop and draws all game objects. This
