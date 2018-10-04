97105d465d44fcff9f52a9b0e958a80f94d190d4
EmilyGarcia <emily1597@gmail.com>
2018-10-04 01:32:37 -0700

    movement

---
+++ woodland_runners-regie_branch/obstacles.js
@@ -0,0 +1,83 @@
+/**
+ * Create the Enemy ship object.
+ */
+function Enemy(object) {
+  console.log("went in the enemy function");
+	var percentFire = .01;
+	var chance = 0;
+  var self = object;
+	this.alive = false;
+
+	/*
+	 * Sets the Enemy values
+	 */
+	this.spawn = function(x, y, speed) {
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.speedX = 0;
+		this.speedY = speed;
+		this.alive = true;
+	};
+
+	/*
+	 * Move the enemy
+	 */
+	this.draw = function() {
+		this.context.clearRect(this.x, this.y, this.width, this.height);
+
+    this.x -= this.speed;
+
+    if(self == "woodenStump" && this.x <= 0 - this.width){
+      return true;
+    }
+    else if(self == "woodenLog" && this.x <= 0 - this.width){
+      return true;
+    }
+    else if(self == "bird" && this.x <= 0 - this.width){
+      return true;
+    }
+    else{
+      if (self === "woodenStump") {
+        this.context.drawImage(imageRepository.woodenStump, this.x, this.y,this.width*(1/10), this.height*(1/10));
+      }
+      else if (self === "woodenLog") {
+        this.context.drawImage(imageRepository.woodenLog, this.x, this.y, this.width*(1/15), this.height*(1/15));
+      }
+      else if (self === "bird") {
+        this.context.drawImage(imageRepository.bird, this.x, this.y, this.width, this.height);
+      }
+      return false;
+    }
+
+
+
+
+
+    // if (self === "woodenStump") {
+    //   this.context.drawImage(imageRepository.woodenStump, this.x, this.y,this.width*(1/10), this.height*(1/10));
+    // }
+    // else if (self === "woodenLog") {
+    //   this.context.drawImage(imageRepository.woodenLog, this.x, this.y, this.width*(1/15), this.height*(1/15));
+    // }
+    // else if (self === "bird") {
+    //   this.context.drawImage(imageRepository.bird, this.x, this.y, this.width, this.height);
+    // }
+
+
+
+	};
+
+	/*
+	 * Resets the enemy values
+	 */
+	this.clear = function() {
+		this.x = 0;
+		this.y = 0;
+		this.speed = 0;
+		this.speedX = 0;
+		this.speedY = 0;
+		this.alive = false;
+	};
+}
+Enemy.prototype = new Drawable();
