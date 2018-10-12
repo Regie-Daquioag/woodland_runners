148d69361db721af49f0e525e84669d41f3b05cb
Regie-Daquioag <rdaquiong@csumb.edu>
2018-10-12 07:39:08 -0700

    got it the game to stop when the objects collide with each other

--- animal.js
+++ animal.js
@@ -93,45 +93,66 @@ function Animal() {
 Animal.prototype = new BaseObject();
 
 
-// // The keycodes that will be mapped when a user presses a button.
-// // Original code by Doug McInnes
-// KEY_CODES = {
-//   38: 'up',
-//   40: 'down',
-// }
-// // Creates the array to hold the KEY_CODES and sets all their values
-// // to false. Checking true/flase is the quickest way to check status
-// // of a key press and which one was pressed when determining
-// // when to move and which direction.
-// KEY_STATUS = {};
-// for (code in KEY_CODES) {
-//   KEY_STATUS[ KEY_CODES[ code ]] = false;
-// }
-// /**
-//  * Sets up the document to listen to onkeydown events (fired when
-//  * any key on the keyboard is pressed down). When a key is pressed,
-//  * it sets the appropriate direction to true to let us know which
-//  * key it was.
-//  */
-// document.onkeydown = function(e) {
-//   // Firefox and opera use charCode instead of keyCode to
-//   // return which key was pressed.
-//   var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-//    if (KEY_CODES[keyCode]) {
-//     e.preventDefault();
-//     KEY_STATUS[KEY_CODES[keyCode]] = true;
-//   }
-// }
-// /**
-//  * Sets up the document to listen to ownkeyup events (fired when
-//  * any key on the keyboard is released). When a key is released,
-//  * it sets teh appropriate direction to false to let us know which
-//  * key it was.
-//  */
-// document.onkeyup = function(e) {
-//   var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-//   if (KEY_CODES[keyCode]) {
-//     e.preventDefault();
-//     KEY_STATUS[KEY_CODES[keyCode]] = false;
-//   }
-// }
+// The keycodes that will be mapped when a user presses a button.
+// Original code by Doug McInnes
+KEY_CODES = {
+  38: 'up',
+  40: 'down',
+}
+// Creates the array to hold the KEY_CODES and sets all their values
+// to false. Checking true/flase is the quickest way to check status
+// of a key press and which one was pressed when determining
+// when to move and which direction.
+KEY_STATUS = {};
+for (code in KEY_CODES) {
+  KEY_STATUS[ KEY_CODES[ code ]] = false;
+}
+/**
+ * Sets up the document to listen to onkeydown events (fired when
+ * any key on the keyboard is pressed down). When a key is pressed,
+ * it sets the appropriate direction to true to let us know which
+ * key it was.
+ */
+document.onkeydown = function(e) {
+  // Firefox and opera use charCode instead of keyCode to
+  // return which key was pressed.
+  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+   if (KEY_CODES[keyCode]) {
+    e.preventDefault();
+    KEY_STATUS[KEY_CODES[keyCode]] = true;
+  }
+}
+/**
+ * Sets up the document to listen to ownkeyup events (fired when
+ * any key on the keyboard is released). When a key is released,
+ * it sets teh appropriate direction to false to let us know which
+ * key it was.
+ */
+document.onkeyup = function(e) {
+  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+  if (KEY_CODES[keyCode]) {
+    e.preventDefault();
+    KEY_STATUS[KEY_CODES[keyCode]] = false;
+  }
+}
+
+
+/*
+var controller = {
+	up:false,
+	down:false,
+
+	keyListener:function(event) {
+		var key_state = (event.type == "keydown")?true:false;
+		switch(event.keyCode) {
+			case 38:// up key
+				controller.up = key_state;
+			break;
+			case 40: // down key
+				controller.down = key_state;
+			break;
+		}
+	}
+};
+
+*/
